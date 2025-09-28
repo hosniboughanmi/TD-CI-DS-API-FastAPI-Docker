bf4a34f2d47e540cd485d42df45fc39c56403add
Hosni Boughanmi <hosniboughanmi@hotmail.fr>
2025-09-27 20:13:37 -0400

    Modification des fichiers test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,11 @@
-def test_smoke():
-    """Test minimal pour vérifier que l’environnement fonctionne."""
-    assert 2 + 2 == 4
+# tests/test_smoke.py
+import pytest
+from httpx import AsyncClient
+from app.main import app
+
+@pytest.mark.anyio
+async def test_smoke():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        resp = await client.get("/")
+    assert resp.status_code == 200
+    assert resp.json() == {"message": "API is up and running!"}
